d536d2f64119abcf540334ccf627def5d4c9f07a
bobcarrolling <bobcarrolling@gmail.com>
2021-02-08 14:32:34 -0500

    Start of home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  ogl = "Show";
 
-  constructor() { }
+  readonly rightarrow = "&#9654;";
+  readonly downarrow = "&#9660;";
+  arrow = this.rightarrow;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  oglToggle() {
+    if (this.ogl === "Show") {
+      this.ogl = "Hide";
+      this.arrow = this.downarrow;
+    } else {
+      this.ogl = "Show";
+      this.arrow = this.rightarrow;
+    }
   }
-
 }
