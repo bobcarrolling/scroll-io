1010d5c5645ffb702990b7aae392ee220f9803a0
bobcarrolling <bobcarrolling@gmail.com>
2021-05-04 15:49:03 -0400

    1 file modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import * as authData from "../auth-data.json";
 
 @Component({
   selector: "app-home",
@@ -9,6 +10,7 @@ export class HomeComponent implements OnInit {
   ogl = "Show";
   localStorage = localStorage;
 
+  authData: any = authData;
 
   readonly rightarrow = "&#9655;";
   readonly downarrow = "&#9661;";
