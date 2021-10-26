c87d367b0da3b82c546a908ed9fc952e40c35d96
bobcarrolling <bobcarrolling@gmail.com>
2021-10-26 14:45:49 -0400

    Class info formatted

--- src/app/classes/classes-display/classes-display.component.ts
+++ src/app/classes/classes-display/classes-display.component.ts
@@ -3,10 +3,9 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-classes-display',
   templateUrl: './classes-display.component.html',
-  styleUrls: ['./classes-display.component.css']
+  styleUrls: ['./classes-display.component.css'],
 })
 export class ClassesDisplayComponent implements OnInit {
-
   private _C;
   @Input()
   get C(): any {
@@ -22,15 +21,51 @@ export class ClassesDisplayComponent implements OnInit {
   }
   set SC(value: any) {
     this._SC = value;
+    this.splitClassName = ('The ' + this.C.name + ' Class').split(" ");
+    this.splitSubclassName = ('Subclass: ' + this.SC.name).split(" ");
   }
 
-  constructor() { }
+  splitClassName: string[];
+  splitSubclassName: string[];
 
-  ngOnInit() {
+  private _showIntro: boolean;
+  @Input()
+  get showIntro(): boolean {
+    return this._showIntro;
   }
+  set showIntro(value: boolean) {
+    this._showIntro = value;
+  }
+  private _showProf: boolean;
+  @Input()
+  get showProf(): boolean {
+    return this._showProf;
+  }
+  set showProf(value: boolean) {
+    this._showProf = value;
+  }
+  private _showTable: boolean;
+  @Input()
+  get showTable(): boolean {
+    return this._showTable;
+  }
+  set showTable(value: boolean) {
+    this._showTable = value;
+  }
+
+  constructor() {}
+
+  ngOnInit() {}
 
   stringify(o: any): string {
     return JSON.stringify(o);
   }
 
+  isWord(word: string) {
+    word = word.toLowerCase();
+    if (word === 'of' || word === 'the') {
+      return false;
+    }
+    return true;
+  }
 }
